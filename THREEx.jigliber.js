c6b1e5404d8327833c9e7f6e7dc831a4edef1de7
Jerome Etienne <jerome.etienne@gmail.com>
2011-09-15 18:58:40 +0200

    better THREEx.jigliber.js

--- THREEx.jigliber.js
+++ THREEx.jigliber.js
@@ -1,19 +1,34 @@
 var THREEx	= THREEx		|| {};
 THREEx.jigliber	= THREEx.jigliber	|| {};
 
-THREEx.jigliber.System	= function()
+THREEx.jigliber	= function()
 {
 	this._system	= jiglib.PhysicsSystem.getInstance();
-	//system.setCollisionSystem(); // CollisionSystemBrite
-	this._system.setCollisionSystem(true); // CollisionSystemGrid
-	//system.setSolverType("FAST");
-	//system.setSolverType("NORMAL");
-	this._system.setSolverType("ACCUMULATED");
-	this._setGravity( new Vector3D( 0, -9.8*2, 0, 0 ) );
 
+	//this._system.setCollisionSystem(); // CollisionSystemBrite
+	this._system.setCollisionSystem(true); // CollisionSystemGrid
+
+	//this._system.setSolverType("FAST");
+	//this._system.setSolverType("NORMAL");
+	this._system.setSolverType("ACCUMULATED");
+
+	this._system.setGravity( new Vector3D( 0, -9.8, 0, 0 ) );
+
+	this._updatedAt	= new Date().getTime();
 }
 
-THREEx.jigliber.addMesh	= function(system, mesh)
+/**
+ * Getter for joglib system
+*/
+THREEx.jigliber.prototype.system	= function()
+{
+	return this._system;
+}
+
+/**
+ * Add a mesh in the system
+*/
+THREEx.jigliber.prototype.addMesh	= function(mesh)
 {
 	var geometry	= mesh.geometry;
 	var body	= null;
@@ -29,8 +44,51 @@ THREEx.jigliber.addMesh	= function(system, mesh)
 	// TODO replace this one by something more specific later
 	mesh.rigidBody = body;
 	
-	system.addBody( body );
+	this._system.addBody( body );
+}
 
+THREEx.jigliber.prototype._integrate	= function()
+{
+	var present	= new Date().getTime();
+	var deltaTime	= present - this._updatedAt;
+	this._updatedAt	= present;
+
+	this._system.integrate( deltaTime / 1000 );				
+}
+
+THREEx.jigliber.prototype._jiglib2three	= function()
+{
+	for ( var i = 0, l = scene.objects.length; i < l; i ++ ) {
+
+		var mesh = scene.objects[i];
+
+		if (mesh.rigidBody) {
+
+			var pos	= mesh.rigidBody.get_currentState().position;
+			var dir = mesh.rigidBody.get_currentState().orientation.get_rawData();
+	
+			var matrix	= new THREE.Matrix4();
+			matrix.setTranslation( pos.x, pos.y, pos.z );
+
+			var rotate	= new THREE.Matrix4(
+				dir[0], dir[1], dir[2], dir[3],
+				dir[4], dir[5], dir[6], dir[7],
+				dir[8], dir[9], dir[10], dir[11],
+				dir[12], dir[13], dir[14], dir[15]
+			);
+			matrix.multiplySelf(rotate);
+
+			mesh.matrix	= matrix;
+			mesh.update(false, true, camera);
+
+		}
+	}
+}
+
+THREEx.jigliber.prototype.update	= function()
+{
+	this._integrate();
+	this._jiglib2three();
 }
 
 THREEx.jigliber._sphere2Body	= function(mesh)
@@ -45,7 +103,7 @@ THREEx.jigliber._sphere2Body	= function(mesh)
 	// build the RigidBody for this Sphere
 	var body	= new jiglib.JSphere( null, radius );
 	// TODO this is bad. to hard that here
-	body.set_mass( 4 / 3 * Math.PI * Math.pow( radius, 3 ) / 100 );
+	body.set_mass( 4 / 3 * Math.PI * Math.pow( radius, 3 ) );
 	body.moveTo( new Vector3D( mesh.position.x, mesh.position.y, mesh.position.z, 0 ) );
 	
 	return body;
